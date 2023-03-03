71f8c6d2947ba0f4ef9148a68d56d399d7f5930a
Robert Brennan <contact@rbren.io>
2023-03-03 10:50:29 -0500

    move test

--- test/test.sh
+++ test/test.sh
@@ -1,14 +1,19 @@
 #! /bin/bash
 
+some_changed=0
 for dir in ./test/*; do
+  if [[ $dir == "./test/test.sh" ]]; then
+    continue
+  fi
   echo $dir
   BAML_FILE=$dir/baml.sh ./transpile.sh > $dir/out.sh
   if ! git diff --quiet --exit-code $dir; then
     echo "test $dir changed"
+    some_changed=1
   fi
 done
 
-if ! git diff --quiet --exit-code ./test; then
+if [[ $some_changed -eq 1 ]]; then
   echo "Some tests changed!"
   exit 1
 fi
